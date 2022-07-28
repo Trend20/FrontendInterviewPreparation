75c8b599d559ffe5236746df167eb35292b0c815
Trend20 <enockomondi305@gmail.com>
2022-07-29 00:50:24 +0300

    stack

---
+++ Stacks/Stack.js
@@ -0,0 +1,13 @@
+/*
+
+STACK (LIFO)
+
+This is an abstract Data Structure that consists of a collection of items
+with two major operations.
+
+push => adds an element to the collection 
+pop => removes the recently added element that hs not been removed.
+
+A peek operation may also give access to the top of the stack without modifying it.
+
+*/
